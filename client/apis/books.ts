2535406d32905a4f77d23704d52d64f14557ee63
Tyaan Singh <singhtyaan@gmail.com>
2024-11-08 10:44:55 +1300

    api-client half done

--- client/apis/books.ts
+++ client/apis/books.ts
@@ -1,6 +1,7 @@
-import { useQueryClient } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import request from 'superagent'
 const rootUrl = '/api/v1'
+
 
 // export function getBooks(): Promise<string[]> {
 //   return request.get(rootUrl + '/fruits').then((res) => {
@@ -8,9 +9,16 @@ const rootUrl = '/api/v1'
 //   })
 // }
 
-export default function checkOutBookEvent(customerId: number, bookId: number){
+export default function useEventBook(id: number) {
   const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: async (values: Event) => {
+      await request.patch(`/api/v1/events/${id}`).send(values)
+    },
+    onSuccess: async () => {
+      queryClient.invalidateQueries({ queryKey: ['event', id] })
+      queryClient.invalidateQueries({ queryKey: ['schedule'] })
+    },
+  })
 }
-
-
-
