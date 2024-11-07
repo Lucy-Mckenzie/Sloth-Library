983b9b8b6bd9ace0f4c7cc25bdf121ffe02dade2
Lucy Mckenzie <lucymckenzie62@gmail.com>
2024-11-08 10:15:11 +1300

    adding component to app.tsx

--- client/routes.tsx
+++ client/routes.tsx
@@ -1,4 +1,5 @@
 import { createRoutesFromElements, Route } from 'react-router-dom'
+import App from './components/App'
 
 export default createRoutesFromElements(
   <Route path="/" element={<App />}>
