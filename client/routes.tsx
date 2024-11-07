45b7ea85b44896535a6d7789830c77ec0c66cb82
Tyaan Singh <singhtyaan@gmail.com>
2024-11-08 10:34:25 +1300

    fixes routing

--- client/routes.tsx
+++
@@ -1,7 +0,0 @@
-import { createRoutesFromElements, Route } from 'react-router-dom'
-import App from './components/App'
-
-export default createRoutesFromElements(
-  <Route path="/" element={<App />}>
-  </Route>
-)
