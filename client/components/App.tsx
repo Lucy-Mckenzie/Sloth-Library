b92deed0e3ce9ef0d97e37bbae36c04248d11844
Lucy Mckenzie <lucymckenzie62@gmail.com>
2024-11-07 22:00:08 +1300

    adding checkout book form for main page

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,16 +1,63 @@
-import { useFruits } from '../hooks/useFruits.ts'
+import { useState, FormEvent, ChangeEvent } from 'react';
 
-function App() {
-  const { data } = useFruits()
-
-  return (
-    <>
-      <div className="app">
-        <h1>Fullstack Boilerplate - with Fruits!</h1>
-        <ul>{data && data.map((fruit) => <li key={fruit}>{fruit}</li>)}</ul>
-      </div>
-    </>
-  )
+interface FormData {
+  customerName: string
+  bookTitle: string
 }
 
-export default App
+interface Props extends FormData {
+  submitLabel: string
+  onSubmit: (_: FormData) => void
+}
+
+export default function CheckoutForm({submitLabel, onSubmit}: Props) {
+  // formState holds current values of form inputs
+  const [formState, setFormState] = useState<FormData>({ 
+    customerName: '',
+    bookTitle: '',
+  })
+
+  // When the user types something, this function is triggered
+  const handleChange = ( event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target
+    setFormState((prev) => ({
+      ...prev,
+      [name]: value,
+    }))
+  } 
+
+  // handleSubmit is called when the form is submitted
+  // It prevents the default form submission and calls onSubmit, sending formState as data to the parent
+  const handleSubmit = (evt: FormEvent) => {
+    evt.preventDefault()
+    onSubmit(formState)
+  } 
+
+    return (
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          Customer name:
+          <input
+            name="customerName"
+            type="text"
+            placeholder="Enter customer name"
+            value={formState.customerName}
+            onChange={handleChange}
+            required />
+        </label>
+
+        <label>
+          Book title:
+          <input
+            name="bookTitle"
+            type="text"
+            placeholder="Enter book title"
+            value={formState.bookTitle}
+            onChange={handleChange} 
+            required />
+        </label>
+        <button type="submit">{submitLabel}</button>
+      </form>
+    );
+  }
